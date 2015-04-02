cc77f1f8fc55115735fd829ab2591553f6c7e76b
Ken Wilcox <wilcoxk@gmail.com>
2015-04-02 15:11:36 -0600

    initial commit

---
+++ RecordingLight/frmMain.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace RecordingLight
+{
+  partial class frmMain
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.btnAlert = new System.Windows.Forms.Button();
+      this.button1 = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // btnAlert
+      // 
+      this.btnAlert.Location = new System.Drawing.Point(12, 12);
+      this.btnAlert.Name = "btnAlert";
+      this.btnAlert.Size = new System.Drawing.Size(75, 23);
+      this.btnAlert.TabIndex = 0;
+      this.btnAlert.Text = "Record";
+      this.btnAlert.UseVisualStyleBackColor = true;
+      this.btnAlert.Click += new System.EventHandler(this.btnAlert_Click);
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(12, 41);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "Off";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // frmMain
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(284, 262);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.btnAlert);
+      this.Name = "frmMain";
+      this.Text = "Recording Light";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button btnAlert;
+    private System.Windows.Forms.Button button1;
+  }
+}
+
---
+++ RecordingLight/frmMain.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Plenom.Components.Busylight.Sdk;
+
+namespace RecordingLight
+{
+  public partial class frmMain : Form
+  {
+    private BusylightLyncController device;
+    private bool die;
+
+    public frmMain()
+    {
+      InitializeComponent();
+      device = new BusylightLyncController();
+      if (!device.IsDeviceAttached())
+      {
+        MessageBox.Show("You don't have a Busylight connected, or I can't see it");
+      }
+    }
+
+    private void Pulse(BusylightColor color, int delayInMiliseconds = 50)
+    {
+      if (delayInMiliseconds < 30) delayInMiliseconds = 30;
+
+      die = false;
+      int holdblue = color.BlueRgbValue;
+      int holdred = color.RedRgbValue;
+      int holdgreen = color.GreenRgbValue;
+
+      while (true)
+      {
+        color.BlueRgbValue -= 10;
+        color.RedRgbValue -= 10;
+        color.GreenRgbValue -= 10;
+
+        if (color.BlueRgbValue < 0)
+          color.BlueRgbValue = holdblue;
+        if (color.RedRgbValue < 0)
+          color.RedRgbValue = holdred;
+        if (color.GreenRgbValue < 0)
+          color.GreenRgbValue = holdgreen;
+
+        device.Light(color);
+        System.Threading.Thread.Sleep(delayInMiliseconds);
+        Application.DoEvents();
+        if (die) break;
+      }
+      device.Light(BusylightColor.Yellow);
+    }
+
+    private void Pulse(List<BusylightColor> colors, int delayInMiliseconds = 50)
+    {
+      if (delayInMiliseconds < 30) delayInMiliseconds = 30;
+
+      die = false;
+      while (true)
+      {
+        foreach (BusylightColor color in colors)
+        {
+          device.Light(color);
+          System.Threading.Thread.Sleep(delayInMiliseconds);
+
+          Application.DoEvents();
+          if (die) break;
+        }
+        if (die) break;
+      }
+      device.Light(BusylightColor.Yellow);
+    }
+
+    private void btnAlert_Click(object sender, EventArgs e)
+    {
+      //Pulse(BusylightColor.Green);
+      //Pulse(new BusylightColor{RedRgbValue = 20, GreenRgbValue = 100, BlueRgbValue = 30});
+      List<BusylightColor> colors = new List<BusylightColor> { BusylightColor.Red, BusylightColor.Off, BusylightColor.Blue, BusylightColor.Off };
+      Pulse(colors, 500);
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      //device.Light(BusylightColor.Off);
+      //device.StopKeepAliveSignaling();
+      die = true;
+    }
+  }
+}
